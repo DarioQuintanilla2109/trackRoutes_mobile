0888fc76f26a990ecbb96ca7663395c7dc037102
Dario_Quintanilla <DarioQuintanilla2@gmail.com>
2021-01-12 15:35:28 -0500

    added signin route, getting back jwt

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext'
+import trackerAPI from '../api/tracker'
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -7,8 +8,36 @@ const authReducer = (state, action) => {
   }
 }
 
+const signup = dispatch => {
+  //make API request to sign up with email and password
+
+  //if we sign up, modify our state
+
+  //if sign up fails, reflect error message
+  return async ({ email, password }) => {
+    try {
+      const resp = await trackerAPI.post('/signup', { email, password })
+      console.log(resp.data)
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+}
+
+const signin = dispatch => {
+  //try to sign in
+  //handle success, update state
+  //handle failure
+  return ({ email, password }) => {}
+}
+
+const signout = dispatch => {
+  //sign out
+  return () => {}
+}
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signout, signup },
   { isSignedIn: false }
 )
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { View, StyleSheet, Keyboard } from 'react-native'
 import { Text, Input, Button } from 'react-native-elements'
 import Spacer from '../components/Spacer'
+import { Context as AuthContext } from '../context/AuthContext'
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext)
+
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
@@ -42,7 +45,12 @@ const SignupScreen = ({ navigation }) => {
         />
       </Spacer>
       <Spacer>
-        <Button style={styles.buttonStyle} title='Sign Up' raised />
+        <Button
+          style={styles.buttonStyle}
+          title='Sign Up'
+          raised
+          onPress={() => signup({ email, password })}
+        />
       </Spacer>
     </View>
   )
