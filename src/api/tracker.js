390ac4da78fc84124aadc4888f3aca42c4fbb01d
Dario_Quintanilla <DarioQuintanilla2@gmail.com>
2021-03-07 17:07:48 -0500

    can fetch list of all tracks and added chevron styling

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const instance = axios.create({
-  baseURL: 'http://4d4cff969472.ngrok.io',
+  baseURL: 'http://bb3c974bb736.ngrok.io',
 })
 
 //if we have token will be automcatically added into our request
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import { View, StyleSheet, Text, Button } from 'react-native'
+import React, { useContext } from 'react'
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native'
+import { NavigationEvents } from 'react-navigation'
+import { Context as TrackContext } from '../context/TrackContext'
+import { ListItem } from 'react-native-elements'
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext)
+  console.log(state)
   return (
     <>
-      <Text>track list screen</Text>
-      <Button
-        title='Go to Track Detail'
-        onPress={() => navigation.navigate('TrackDetail')}
+      <NavigationEvents onWillFocus={fetchTracks} />
+      <FlatList
+        data={state}
+        keyExtractor={item => item._id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity>
+              <ListItem bottomDivider>
+                <ListItem.Content>
+                  <ListItem.Title>{item.name}</ListItem.Title>
+                </ListItem.Content>
+                <ListItem.Chevron />
+              </ListItem>
+            </TouchableOpacity>
+          )
+        }}
       />
     </>
   )
